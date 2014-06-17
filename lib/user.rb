f761d17aaf452c4c51457ced1a287b49f25ac216
Faezrah Rizalman <fwrizalman@gmail.com>
2014-06-17 18:05:54 +0100

    Added password confirmation

--- lib/user.rb
+++ lib/user.rb
@@ -20,7 +20,18 @@ class User
   # database instead of the plain password for security reasons.
 
   def password=(password)
+    @password = password
   	self.password_digest = BCrypt::Password.create(password)
   end
+
+  attr_reader :password
+  attr_accessor :password_confirmation
+
+  # this is datamapper's method of validating the model
+  # the model will not be saved unless both password
+  # and password_confirmation are the same
+  # read more about it at http://datamapper.org/docs/validations.html
+
+  validates_confirmation_of :password
 
 end
