f0f7350d9880f6fcb6b733c181695ebaa1bf8f82
Faezrah Rizalman <fwrizalman@gmail.com>
2014-06-17 17:28:25 +0100

    Added user accounts and cleaned up the codebase

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,6 +1,3 @@
-require 'sinatra'
-require 'data_mapper'
-
 env = ENV['RACK_ENV'] || "development"
 
 # we're telling datamapper to use a postgres database on localhost. 
@@ -9,6 +6,7 @@ DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link' # this needs to be done after datamapper is initialised
 require './lib/tag' # this needs to be done after datamapper is initialised
+require './lib/user'
 
 
 # after declaring your models, you should finalise them
@@ -16,22 +14,3 @@ DataMapper.finalize
 
 # however, the database tables don't exist yet. let's tell datamapper to create them
 DataMapper.auto_upgrade!
-
-get '/' do
-	@links = Link.all
-	erb	:index
-end
-
-post '/links' do
-	url = params["url"]
-	title = params["title"]
-	tags = params["tags"].split(" ").map { |tag| Tag.first_or_create(:text => tag) }
-	Link.create(:url => url, :title => title, :tags => tags)
-	redirect to ('/')
-end
-
-get '/tags/:text' do
-	tag = Tag.first(:text => params[:text])
-	@links = tag ? tag.links : []
-	erb :index
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ ENV['RACK_ENV'] = 'test' # because we need to know what database to work with
 # what environment it's running it: test or development.
 # the environment determines what database to use.
 
-require './server'
+require './app/server'
 require 'database_cleaner'
 require 'capybara/rspec'
 
