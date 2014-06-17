43cbaea2aba8a130b88f8eef54cf4a4f7a6182a2
Faezrah Rizalman <fwrizalman@gmail.com>
2014-06-17 16:06:22 +0100

    Filtering by tag

--- server.rb
+++ server.rb
@@ -25,7 +25,13 @@ end
 post '/links' do
 	url = params["url"]
 	title = params["title"]
-	tags = params["tags"]
+	tags = params["tags"].split(" ").map { |tag| Tag.first_or_create(:text => tag) }
 	Link.create(:url => url, :title => title, :tags => tags)
 	redirect to ('/')
+end
+
+get '/tags/:text' do
+	tag = Tag.first(:text => params[:text])
+	@links = tag ? tag.links : []
+	erb :index
 end
