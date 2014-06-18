56a3eadd1d64eaea039a9a4a63a359973aaa02cb
Faezrah Rizalman <fwrizalman@gmail.com>
2014-06-18 12:16:45 +0100

    Added a feature to handle input errors and installed rack-flash gem

--- app/server.rb
+++ app/server.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'data_mapper'
+require 'rack-flash'
 require './lib/link'
 require './lib/tag'
 require './lib/user'
@@ -8,6 +9,7 @@ require_relative 'data_mapper_setup'
 
 enable :sessions
 set :session_secret, 'super secret'
+use Rack::Flash
 
 get '/' do
 	@links = Link.all
@@ -33,11 +35,17 @@ get '/users/new' do
 	# we need the quotes because otherwise
 	# ruby would divide the symbol :users by the
 	# variable new (which makes no sense)
+	@user = User.new
 	erb :"users/new"
 end
 
 post '/users' do
-	user = User.create(:email => params[:email], :password => params[:password], :password_confirmation => params[:password_confirmation])
-	session[:user_id] = user.id
-	redirect to('/')
+	@user = User.new(:email => params[:email], :password => params[:password], :password_confirmation => params[:password_confirmation])
+	if @user.save
+		session[:user_id] = @user.id
+		redirect to('/')
+	else
+		flash[:notice] = "Sorry, your passwords don't match"
+		erb :"users/new"
+	end
 end
